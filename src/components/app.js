c9fc4adc1090c06980dc393ea4e68f917dffe6c8
zaturnvalley <zacharycmason@gmail.com>
2016-11-07 14:22:56 -0800

    todos-list component

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const todos = [
+{ 
+  task: 'apply to 5 jobs',
+  isCompleted: true
+},
+{
+  task: 'walk marley 2 times',
+  isCompleted: false
+}
+]
+
 export default class App extends React.Component {
   render() {
     return (
---
+++ src/components/todos_list.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class TodosList extends React.Component {
+  render(){
+    return (
+      );
+  }
+}
