d6ecb785e0e165b1e406235dc4657255f8a1cc72
zaturnvalley <zacharycmason@gmail.com>
2016-11-08 12:30:59 -0800

    pulled in merge and path

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var path = require('path');
+var merge = require('merge');
 
-gulp.task('default', function() {
-  return console.log('task ran');
-  // return gulp.src('./src/*.js')
-  //   .pipe(gulp.dest(/))
-});
+gulp.task('scripts', function(){
+  return gulp.src('./src/*.js', './src/components/*.js')
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('./dist/'));
+})
+
+gulp.task('default', ['scripts']);
