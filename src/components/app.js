41775c93f1967c522239fb30198a59d91feffa59
zaturnvalley <zacharycmason@gmail.com>
2016-11-07 16:05:07 -0800

    value from input updates to dom

--- src/components/app.js
+++ src/components/app.js
@@ -25,9 +25,18 @@ export default class App extends React.Component {
     return (
       <div>
         <h1>React To Do App</h1>
-        <CreateToDo />
-        <TodosList todos={this.state.todos}/>
+        <CreateToDo createTask={this.createTask.bind(this)} />
+        <TodosList 
+          todos={this.state.todos}
+          />
       </div>
     );
+  }
+  createTask(task) {
+    this.state.todos.push({
+      task,
+      isCompleted: false
+    });
+    this.setState({ todos: this.state.todos });
   }
 }
--- src/components/create_todo.js
+++ src/components/create_todo.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
-export default class CreateToDo extends React.Component {
+export default class TodosList extends React.Component {
   render() {
     return (
-      <form className='form-group'>
-        <input  className="form-control" type="text" placeholder="What do I need to do?" />
+      <form onSubmit={this.handleCreate.bind(this)} className='form-group'>
+        <input  className="form-control" type="text" 
+        placeholder="What do I need to do?" ref="createInput" />
         <span className="input-group-btn">
           <button className="btn btn-info">Create</button>
         </span>
       </form>
       );
+  }
+  handleCreate(e) {
+    e.preventDefault();
+    this.props.createTask(this.refs.createInput.value);
   }
 }
