d5edd147b6fccbafa14220e85f068ba8dd6b8ee3
zaturnvalley <zacharycmason@gmail.com>
2016-11-08 12:55:20 -0800

    created gulp bundle.js file, testing

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,18 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var path = require('path');
 var merge = require('merge');
+var uglify = require('gulp-uglify');
+
+var folders = ['./src', './src/components']
 
 gulp.task('scripts', function(){
-  return gulp.src('./src/*.js', './src/components/*.js')
-    .pipe(concat('bundle.js'))
-    .pipe(gulp.dest('./dist/'));
-})
+  var tasks = folders.map(function(element){
+    return gulp.src(element + '/*.js')
+      .pipe(concat('bundle.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('./'));
+  });
+  return merge(tasks);
+});
 
 gulp.task('default', ['scripts']);
